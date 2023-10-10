e49d1e821d35b4b484974630c7941caf13fbd2b3
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2023-10-10 18:16:35 +0200

    Change next and previous buttons and intro text to geman

--- js/jspsych/poldrack_plugins/jspsych-poldrack-instructions.js
+++ js/jspsych/poldrack_plugins/jspsych-poldrack-instructions.js
@@ -44,17 +44,17 @@ jsPsych.plugins['poldrack-instructions'] = (function() {
           nav_html += "<button id='jspsych-instructions-next'>End Instructions</button>"
         } else {
           if (current_page == 0) {
-            nav_html += "<button id='jspsych-instructions-next'>Next</button>"
+            nav_html += "<button id='jspsych-instructions-next'>Weiter</button>"
           } else if (current_page == trial.pages.length - 1) {
             if (trial.allow_backward) {
-              nav_html += "<button id='jspsych-instructions-back'>Previous</button>";
+              nav_html += "<button id='jspsych-instructions-back'>Zurück</button>";
             }
             nav_html += "<button id='jspsych-instructions-next'>End Instructions</button>"
           } else {
             if (trial.allow_backward) {
-              nav_html += "<button id='jspsych-instructions-back'>Previous</button>";
+              nav_html += "<button id='jspsych-instructions-back'>Zurück</button>";
             }
-            nav_html += "<button id='jspsych-instructions-next'>Next</button>"
+            nav_html += "<button id='jspsych-instructions-next'>Weiter</button>"
           }
         }
         nav_html += "</div>"
